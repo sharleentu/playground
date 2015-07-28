4071c9e10ff0a28be9123824e56c9469d888c897
Sharleen Tu <sharleen@Sharleens-MacBook-Air.local>
2015-07-28 10:59:44 -0700

    created initial app

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularTestApp
+ * @description
+ * # angularTestApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularTestApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularTestApp
+ */
+angular.module('angularTestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
